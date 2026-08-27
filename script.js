f61150a0eeebefe2358c3f8db77def071db01834
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-08-27 21:21:31 +0100

    add a live search input

--- script.js
+++ script.js
@@ -7,17 +7,38 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  rootElem.innerHTML = `${rootElem.textContent}<span class="credit"><span class="creative-commons">CC</span> Data source at <a href="https://tvmaze.com/">TVmaze API</a> under <a href="http://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> license.</span>`;
 
-  for (const i of episodeList) {
-    const card = document.getElementById("template-episode").content.cloneNode(true);
-    card.querySelector("article").id = i.id;
-    card.querySelector("h3").textContent = `${i.name} - S${i.season.toString().padStart(2, "0")}E${i.number.toString().padStart(2, "0")}`;
-    card.querySelector("img").src = i.image.medium;
-    //card.querySelector("img").alt = "Game of Thrones 1x01";
-    card.querySelector("p").outerHTML = i.summary;
-    document.querySelector("main").append(card);
+  const searchTerm = document.getElementById("search").value.toLowerCase();
+  const episodes = episodeList.filter((episode) => episode.name.toLowerCase().includes(searchTerm) || episode.summary.toLowerCase().includes(searchTerm));
+  const cards = episodes.map(makeCardForEpisode);
+  const sectionElem = document.querySelector("section"); 
+  while (sectionElem.firstChild) { 
+    sectionElem.firstChild.remove(); 
   }
+  document.querySelector("section").append(...cards);
+  document.getElementById("found").innerHTML = `Displaying ${episodes.length}/${episodeList.length} episodes`;
 }
+
+function makeCardForEpisode(episode) {
+    const card = document.getElementById("template-episode").content.cloneNode(true);
+
+    card.querySelector("article").id = episode.id;
+    card.querySelector("h3").textContent = `${episode.name} - S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+    card.querySelector("img").src = episode.image.medium;
+    //card.querySelector("img").alt = "Game of Thrones 1x01";
+    card.querySelector("p").outerHTML = episode.summary;
+
+    return card;
+}
+
+/*function searchInputHandler(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  console.log(searchTerm);
+  setup();
+}*/
+
+window.addEventListener("load", () => {
+  document.getElementById("search").addEventListener("input", setup);
+});
 
 window.onload = setup;
